9aa001cf195aa77f79a95cd129453444939d63f7
irsyadans <irsyad.ansori321@gmail.com>
2023-06-01 21:15:49 +0700

    update ke-3 pipeline

--- routes.js
+++ routes.js
@@ -2,28 +2,28 @@ const routes = [{
         method: '*',
         path: '/',
         handler: (req, res) => {
-            return 'Halaman tidak dapat diakses dengan method tersebut versi CI/CD'
+            return 'Halaman tidak dapat diakses dengan method tersebut ~ versi CI/CD'
         },
     },
     {
         method: 'GET',
         path: '/',
         handler: (req, res) => {
-            return 'Homepage versi CI/CD'
+            return 'Homepage ~ versi CI/CD'
         },
     },
     {
         method: 'GET',
         path: '/about',
         handler: (req, res) => {
-            return 'About page versi CI/CD'
+            return 'About page ~ versi CI/CD'
         },
     },
     {
         method: 'GET',
         path: '/{any*}',
         handler: (req, res) => {
-            return 'Halaman tidak ditemukan versi CI/CD'
+            return 'Halaman tidak ditemukan ~ versi CI/CD'
         }
     }
 ]
